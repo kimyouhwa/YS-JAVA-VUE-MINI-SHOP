8d7f6891c82c19155223fcb78ca23044fb4bef0e
kimyouhwa <s_fan@naver.com>
2024-08-06 09:50:34 +0900

    라우터설정1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderArea/>
+  <router-view/>
+  <FooterArea/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { defineComponent } from '@vue/composition-api';
+import HeaderArea from './layouts/HeaderArea.vue';
+import FooterArea from './layouts/FooterArea.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{HeaderArea, FooterArea},
+  setup() {
+    
+  },
 }
 </script>
+
 
 <style>
 #app {
@@ -21,6 +25,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
